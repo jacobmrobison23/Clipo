1b416a034d358c875971f9f130f8b40c18aae467
Eliot Cho <eliotcho99@gmail.com>
2024-09-06 15:17:28 -0400

    user controller is added, deleted some lines in user routes, check if both are updated

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -7,10 +7,6 @@ const {
   updateUser,
 } = require("../../controllers/user-controller");
 
-// import middleware
-const { authMiddleware } = require("../../utils/auth");
-
-
 
 router.route("/profile/:username").get(protectRoute, getUserProfile);
 
