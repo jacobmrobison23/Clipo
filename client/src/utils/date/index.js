b0aaeb1689b1ac1326f38d9e191a7248e1cd068e
Eliot Cho <eliotcho99@gmail.com>
2024-09-10 15:11:43 -0400

    date, format months

--- client/src/utils/date/index.js
+++ client/src/utils/date/index.js
@@ -16,6 +16,27 @@ export const formatPostDate = (createdAt) => {
 	} else if (timeDifferenceInMinutes >= 1) {
 		return `${timeDifferenceInMinutes}m`;
 	} else {
-		return "Just now";
+		return "Now";
 	}
+};
+
+export const formatMemberSinceDate = (createdAt) => {
+	const date = new Date(createdAt);
+	const months = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	];
+	const month = months[date.getMonth()];
+	const year = date.getFullYear();
+	return `Joined ${month} ${year}`;
 };
